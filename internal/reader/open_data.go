8ced392545ed6e9c4a60b8ae8a6d6cf51c7c3658
MikelGV <mikelgaldosv@gmail.com>
2025-10-11 02:36:01 +0200

    WIP: testing how gota works and thinking in how to open the file detect the file type and loading the data

--- internal/reader/open_data.go
+++ internal/reader/open_data.go
@@ -1,43 +1,79 @@
 package reader
 
 import (
-	"encoding/csv"
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
+
+	"github.com/go-gota/gota/dataframe"
 )
 
 /**
     I need to be able to automatically detect the csv's tables.
     Also I need to detect the file type.
+type File_Handler struct {
+FilenName string
+}
 **/
 
-type File_Handler struct {
-    FilenName string
-}
 
-func (f *File_Handler) OpenFile() ( *os.File, error){
-    /**
-        I need to add a way to detect if the file is supported or not, and if it's
-        not a csv, tsv, or json throw an error and close the connection
-    **/
-    file, err := os.Open(f.FilenName)
+func OpenFile() ( *os.File, error){
+    file, err := os.Open("../testing_data/dog_breeds.csv")
     if err != nil {
         return nil, fmt.Errorf("Error file name not found or not provided: %v", err)
     }
 
-    if err := detectFileType(file); err != nil {
-        file.Close()
-        return nil, fmt.Errorf("Error file type doesn't match our valid file types: %s", err)
-    }
-
-
     return file, nil 
 }
 
-func detectFileType(file *os.File) error {
+func DetectFileType() (*os.File, error) {
+    file, err := OpenFile()
+    if err != nil {
+        return nil, fmt.Errorf("Error opening file: %s", err)
+    }
+    reader := bufio.NewReader(file)
+
+    content, err := reader.ReadString('\n')
+    if err != nil {
+        return nil, fmt.Errorf("Error reading file: %s", err)
+    }
+
+    if isJSON(content) {
+        f := dataframe.ReadJSON(file)
+        fmt.Println(f)
+    } else if isCSV(content) {
+        f := dataframe.ReadCSV(file)
+        fmt.Println(f)
+    } else if isTSV(content) {
+        f := dataframe.ReadCSV(file)
+        fmt.Println(f)
+    } else {
+        return nil, fmt.Errorf("file is in a wrong format")
+    }
+    return file, nil
 }
 
-func isJSON() {}
+func isJSON(content string) bool {
+    var js json.RawMessage
+    return json.Unmarshal([]byte(content), &js) == nil
+}
 
-func isTSVorCSV() {}
+func isTSV(content string) bool {
+    return countOcurrence(content, '\t') > 0
+}
+
+func isCSV(content string) bool {
+    return countOcurrence(content, ',') > 0
+}
+
+func countOcurrence(content string, char rune) int {
+    count := 0
+    for _, c := range content {
+        if c == char {
+            count++
+        }
+    }
+    return count
+}
 
--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-gota/gota/dataframe"
+)
+
 func main() {
+
+    file, err := os.Open("./testing_data/dog_breeds.csv")
+    if err != nil {
+        fmt.Printf("Error file name not found or not provided: %v", err)
+    }
+
+    df := dataframe.ReadCSV(file)
+
+
+    fmt.Println(df)
 }
