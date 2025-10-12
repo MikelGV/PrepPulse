504248c2ab349bc71923767ced00c929a3ccbcdd
MikelGV <mikelgaldosv@gmail.com>
2025-10-12 09:12:10 +0200

    WIP: pagination component file added, and some of the model and update logic added

--- cmd/tui/model.go
+++ cmd/tui/model.go
@@ -1,15 +1,23 @@
 package tui
 
 import (
-    tea "github.com/charmbracelet/bubbletea"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/go-gota/gota/dataframe"
 )
 
 type Model struct {
+    df *dataframe.DataFrame
+    err error
 }
 
-func (m *Model) NewModel() {
+func NewModel(df *dataframe.DataFrame) Model {
+    //ctx, cancel := context.WithCancel(context.Background())
+    return Model{
+        df: df,
+    }
 }
 
-func (m *Model) Init() tea.Cmd{
+func (m Model) Init() tea.Cmd{
     return tea.Batch(tea.EnterAltScreen)
 }
--- cmd/tui/update.go
+++ cmd/tui/update.go
@@ -2,6 +2,27 @@ package tui
 
 import tea "github.com/charmbracelet/bubbletea"
 
-func (m Model) Update() (tea.Model, tea.Cmd) {
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+    /**
+        I have to add a case for when a dataframe is sent, when and a few cases 
+        for when something happens(e.g. querying/filtering something)
+    **/
+        case error:
+            m.err = msg
+            return m, nil
+
+        case tea.KeyMsg:
+            switch msg.String() {
+                case "q", "ctrl+c":
+                    return m, tea.Quit
+            }
+    }
+
     return m, nil
+}
+
+func waitForDataFrames(m Model) tea.Cmd {
+    return func() tea.Msg {
+    }
 }
