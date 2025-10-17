7882f76915618d8eeff1dd8fbe2f3adfec9ede0c
MikelGV <mikelgaldosv@gmail.com>
2025-10-17 09:45:04 +0200

    WIP: added error caching in the view

--- cmd/tui/view.go
+++ cmd/tui/view.go
@@ -2,8 +2,19 @@ package tui
 
 import "github.com/charmbracelet/lipgloss"
 
+var (
+    errorStyle = lipgloss.NewStyle().
+        Foreground(lipgloss.Color("#DB222A")).
+        Padding(0, 1)
+)
+
 func (m Model) View() string {
     var output []string
-    output = append(output, m.table.Render())
+
+    if m.err != nil {
+        output = append(output, errorStyle.Render("Something went wrong opening file"))
+    } else {
+        output = append(output, m.table.Render())
+    }
     return lipgloss.JoinVertical(lipgloss.Center, output...) 
 }
